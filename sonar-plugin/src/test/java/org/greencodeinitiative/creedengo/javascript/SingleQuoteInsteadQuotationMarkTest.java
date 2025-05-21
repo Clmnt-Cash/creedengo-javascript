9d572c317a1d223dcab3c92d917e4ec0c35057b9
Clmnt-Cash <clement.cachet@edu.ece.fr>
2025-05-21 10:17:06 +0200

    java file

---
+++ sonar-plugin/src/test/java/org/greencodeinitiative/creedengo/javascript/SingleQuoteInsteadQuotationMarkTest.java
@@ -0,0 +1,19 @@
+package org.greencodeinitiative.creedengo.javascript;
+
+import org.sonar.plugins.javascript.api.JavaScriptCheck;
+import org.sonar.plugins.javascript.api.JavaScriptRule;
+
+public class SingleQuoteInsteadQuotationMarkTest {
+    public static final String MESSAGE = "Use single quotes instead of double quotes for strings.";
+    public static final String RULE_KEY = "SingleQuoteInsteadQuotationMark";
+    public static final String REPOSITORY_KEY = "creedengo-javascript";
+    public static final String REPOSITORY_NAME = "Creedengo JavaScript Rules";
+
+    public static final String RULE_NAME = "Single Quote Instead Quotation Mark";
+
+    public static final Class<? extends JavaScriptCheck> CHECK_CLASS = SingleQuoteInsteadQuotationMarkCheck.class;
+
+    private SingleQuoteInsteadQuotationMarkTest() {
+        // Prevent instantiation
+    }
+}
